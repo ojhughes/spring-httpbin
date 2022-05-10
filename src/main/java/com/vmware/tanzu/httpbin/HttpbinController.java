0e43a81b44b3670cfa11370b3490e3a196d7d5a2
Ollie Hughes <ohughes@pivotal.io>
2022-05-10 13:45:00 +0100

    add /get and /post endpoints wip - need to fix post with raw data and file

--- src/main/java/com/vmware/tanzu/httpbin/HttpbinController.java
+++ src/main/java/com/vmware/tanzu/httpbin/HttpbinController.java
@@ -1,20 +1,19 @@
 package com.vmware.tanzu.httpbin;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Map;
 
-import static io.netty.handler.codec.http.HttpHeaders.Values.APPLICATION_JSON;
+import static io.netty.handler.codec.http.HttpHeaders.Values.*;
 
 @RestController
 public class HttpbinController {
@@ -33,22 +32,76 @@ public class HttpbinController {
 
     @GetMapping(path = "/headers", produces = APPLICATION_JSON)
     public Mono<String> headers(ServerWebExchange exchange) throws JsonProcessingException {
-        Map<String, String> headerMap = exchange
-                .getRequest()
-                .getHeaders()
-                .toSingleValueMap();
+        var headerMap = getHeaderMap(exchange);
         return Mono.just(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(headerMap));
     }
 
     @GetMapping(path = "/ip", produces = APPLICATION_JSON)
     public Mono<String> ip(ServerWebExchange exchange)  {
+        return Mono.just(objectMapper.createObjectNode().put("origin", getRemoteAddress(exchange)).toString());
+    }
+
+    @GetMapping(path = "/get", produces = APPLICATION_JSON)
+    public Mono<String> get(@RequestParam Map<String,String> params, ServerWebExchange exchange) throws JsonProcessingException {
+        ObjectNode response = buildResponseJson(objectMapper, params, getHeaderMap(exchange), getRemoteAddress(exchange), exchange);
+        return Mono.just(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
+    }
+
+    @PostMapping(path = "/post", produces = APPLICATION_JSON, consumes = "application/x-www-form-urlencoded")
+    public Mono<String> postForm(@RequestParam Map<String,String> params, ServerWebExchange exchange) throws JsonProcessingException {
+        ObjectNode response = buildResponseJson(objectMapper, params, getHeaderMap(exchange), getRemoteAddress(exchange), exchange);
+        response.set("data",  objectMapper.createObjectNode());
+        response.set("json",  objectMapper.createObjectNode());
+        return exchange.getFormData()
+               .map(MultiValueMap::toSingleValueMap)
+               .map(formData -> response.set("form", objectMapper.valueToTree(formData)).toString());
+    }
+
+    @PostMapping(path = "/post", produces = APPLICATION_JSON, consumes = "*/*")
+    public Mono<String> postData(@RequestParam Map<String,String> params, @RequestBody String body, ServerWebExchange exchange) throws JsonProcessingException {
+        ObjectNode response = buildResponseJson(objectMapper, params, getHeaderMap(exchange), getRemoteAddress(exchange), exchange);
+        response.put("data", body);
+        response.set("form",  objectMapper.createObjectNode());
+        response.set("json",  objectMapper.createObjectNode());
+        return Mono.just(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
+
+    }
+
+    @PostMapping(path = "/post", produces = APPLICATION_JSON, consumes = APPLICATION_JSON)
+    public Mono<String> postJson(@RequestParam Map<String,String> params, @RequestBody JsonNode body, ServerWebExchange exchange) throws JsonProcessingException {
+        ObjectNode response = buildResponseJson(objectMapper, params, getHeaderMap(exchange), getRemoteAddress(exchange), exchange);
+        response.set("json", body);
+        response.set("form",  objectMapper.createObjectNode());
+        response.set("data",  objectMapper.createObjectNode());
+        return Mono.just(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response));
+    }
+
+    private ObjectNode buildResponseJson(ObjectMapper objectMapper, Map<String, String> params, Map<String, String> exchange, String exchange1, ServerWebExchange exchange2) {
+        var response = objectMapper.createObjectNode();
+        if(!params.isEmpty()){
+            response.set("args", objectMapper.valueToTree(params));
+        }
+        response.set("headers", objectMapper.valueToTree(exchange));
+        response.put("origin", exchange1);
+        response.put("url", exchange2.getRequest().getURI().toString());
+        return response;
+    }
+
+    private Map<String, String> getHeaderMap(ServerWebExchange exchange) {
+        return exchange
+                .getRequest()
+                .getHeaders()
+                .toSingleValueMap();
+    }
+
+    private String getRemoteAddress(ServerWebExchange exchange) {
         String remoteAddress;
-        if(exchange.getRequest().getLocalAddress().getHostString().equals("0:0:0:0:0:0:0:1")) {
+        if(exchange.getRequest().getRemoteAddress().getHostString().equals("0:0:0:0:0:0:0:1")) {
             remoteAddress = "127.0.0.1";
         }
         else {
             remoteAddress = exchange.getRequest().getLocalAddress().getHostString();
         }
-        return Mono.just(objectMapper.createObjectNode().put("origin", remoteAddress).toString());
+        return remoteAddress;
     }
 }
